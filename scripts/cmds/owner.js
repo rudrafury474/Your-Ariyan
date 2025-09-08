ad85ff0f26d054d413453ec8f324f3718e276d1c
rudrafury474 <konamirudro@gmail.com>
2025-09-08 22:11:14 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -16,15 +16,13 @@ module.exports = {
 		envConfig: {}
 	},
 	onStart: async function ({ message }) {
-		const authorName = " ⩸_ 𝙰𝚁𝙸𝚈𝙰𝙽 _⩸ ";
-		const ownAge = "『 ⩸_ 19+ _⩸ 』";
-		const messenger = " //m.me/ariyan12hi ";
-		const authorFB = " https://www.facebook.com/profile.php?id=61578434375227 ";
-		const authorNumber = "018××××××*";
+		const authorName = " ⩸_ RUDRAA FURY _⩸ ";
+		const ownAge = "『 ⩸_ 16+ _⩸ 』";
+		const messenger = " //m.me/RUDRAfury ";
+		const authorFB = "";
+		const authorNumber = "015860450×**";
 		const Status = "⩸_ 𝚂𝙸𝙽𝙶𝙻𝙴 _⩸";
-		const urls = [
-"https://drive.google.com/uc?export=download&id=16B-IPuC_Zv6ZtVr9rtdEI3fOtkXfiz0d",
-"https://drive.google.com/uc?export=download&id=16B-IPuC_Zv6ZtVr9rtdEI3fOtkXfiz0d"
+		const urls = ["
 ];
 		const link = urls[Math.floor(Math.random() * urls.length)];
 		const now = moment().tz('Asia/Jakarta');
@@ -41,17 +39,17 @@ module.exports = {
 			body: `✨《 𝐁𝐨𝐭 𝐀𝐧𝐝 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 》🎀
 \🤖彡𝐵𝑜𝑡 𝑁𝑎𝑚𝑒 :  ${global.GoatBot.config.nickNameBot}
 \👾彡𝐵𝑜𝑡 𝑆𝑦𝑠𝑡𝑒𝑚 𝑃𝑟𝑒𝑓𝑖𝑥 : ${global.GoatBot.config.prefix}
-\💙彡𝑂𝑤𝑛𝑒𝑟 𝑁𝑎𝑚𝑒 : ${authorName}
-\📝彡𝐴𝑔𝑒  : ${ownAge}
-\💕彡𝑅𝑒𝑙𝑎𝑡𝑖𝑜𝑛𝑆ℎ𝑖𝑝: ${Status}
+\💙彡𝑂𝑤𝑛𝑒𝑟 𝑁𝑎𝑚𝑒 : ${Rudra Fury}
+\📝彡𝐴𝑔𝑒  : ${16}
+\💕彡𝑅𝑒𝑙𝑎𝑡𝑖𝑜𝑛𝑆ℎ𝑖𝑝: ${single}
 \🌐彡𝑊𝑝 : ${authorNumber}
 \🌍彡𝐹𝑎𝑐𝑒𝑏𝑜𝑜𝑘 𝐿𝑖𝑛𝑘 :  ${authorFB}
 \🗓彡𝐷𝑎𝑡𝑒 : ${date}
 \⏰彡𝑁𝑜𝑤 𝑇𝑖𝑚𝑒 : ${time}
 \🔰彡𝐴𝑛𝑦 𝐻𝑒𝑙𝑝 𝐶𝑜𝑛𝑡𝑎𝑐𝑡 : ${messenger}__⩸
 \📛彡𝐵𝑜𝑡 𝐼𝑠 𝑅𝑢𝑛𝑛𝑖𝑛𝑔 𝐹𝑜𝑟 : ${uptimeString}
-    𝑻𝒈:  //t.me/nisan_editz
-    𝑰𝒏𝒔𝒕𝒂:  //www.instagram.com/im_editz_x2.0?
+    𝑻𝒈:  i don't have
+    𝑰𝒏𝒔𝒕𝒂: rudraa fury
 \===============`,
 			attachment: await global.utils.getStreamFromURL(link)
 		});
